ca3cf8f9541f829c14de6e68ee62b3141fa483c6
akifk11 <69064758+akifk11@users.noreply.github.com>
2022-01-10 18:06:47 +0300

    patikaodev1

---
+++ arrayfunction/index.js
@@ -0,0 +1,30 @@
+const dizim = [
+    {
+        name:"akif",
+        age:21,
+    },
+    {
+        name:"dilruba",
+        age:25,
+    },
+    {
+        name:"rana",
+        age:19,
+    },
+];
+
+// const result=dizim.find((item)=>item.name==="akif"&& item.age>20);
+// console.log(result);
+
+// const filtrele=dizim.filter((item)=>item.name==='akif');
+// console.log(filtrele);
+
+// const some = dizim.some(item=>item.age===21);
+// console.log(some);
+
+// const every=dizim.every((item)=>item.age>17);
+// console.log(every);
+
+const meyveler=["elma","armut","muz"];
+const isInculed=meyveler.includes("muz");
+console.log(isInculed);
